5c5f0a84147d6e842842d9a3e606457b7a1ebf43
joe <jpiro@opentext.com>
2019-05-09 16:05:40 -0400

    activated models and added visitors page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, visitors
 
 urlpatterns = [
-    path('', home, name='home_page')
+    path('', home, name='home_page'),
+    path('visitors', visitors, name='visitors_page')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import HttpResponse
+from django.template import loader
+
+from .models import Visitor
 
 def home(request):
     return HttpResponse('<h1>Hello there!</h1>')
+
+def visitors(request):
+    latest_visitors = Visitor.objects.all()
+    template = loader.get_template('pages/index.html')
+    context = { 'latest_visitor_list' : latest_visitors }
+    return HttpResponse(template.render(context,request))
